d73da8e3265ba75f29026b6c642c07814115c394
Jeremy Addy <addyjj@outlook.com>
2026-02-13 14:29:21 -0600

    move ioc to console program.cs

--- Weather.Console/Program.cs
+++ Weather.Console/Program.cs
@@ -2,10 +2,13 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Refit;
 using Weather.Core.Interfaces;
 using Weather.Core.Options;
 using Weather.Core.Services;
-using Weather.Infrastructure.Ioc;
+using Weather.Infrastructure.Entity;
+using Weather.Infrastructure.Entity.Contexts;
+using Weather.Infrastructure.Rest;
 
 // Build IConfiguration
 var configBuilder = new ConfigurationBuilder()
@@ -29,7 +32,33 @@ services.Configure<DatabaseOptions>(options =>
     options.ConnectionString = config.GetConnectionString("sql") ?? "";
 });
 
-services.RegisterServices(config);
+// Context
+services.AddScoped<WeatherContext>();
+
+// Services
+services.AddScoped<IWeatherService, WeatherService>();
+
+// Repositories
+services.AddScoped<IAmbientWeatherRepository, AmbientWeatherRepository>();
+services.AddScoped<IWeatherDataRepository, WeatherDataRepository>();
+
+// Options
+services.Configure<AmbientWeatherOptions>(
+    config.GetSection("Ambient"));
+
+// Refit Client - works for both console and web
+services.AddTransient<AmbientWeatherHttpHandler>();
+
+services.AddRefitClient<IAmbientWeatherApi>()
+    .AddHttpMessageHandler<AmbientWeatherHttpHandler>()
+    .ConfigureHttpClient((sp, c) =>
+    {
+        var options = sp.GetRequiredService<IOptions<AmbientWeatherOptions>>().Value;
+        c.BaseAddress = new Uri(options.ApiUrl);
+    });
+
+// Logger
+services.AddLogging(builder => builder.AddConsole());
 var serviceProvider = services.BuildServiceProvider();
 
 // Get services
